ead498015db29817792a108c25dc1dfca199e8bf
Mohammed Mustafa <mustuahmedrayan@gmail.com>
2026-03-10 22:40:11 -0400

    fixed the checklist bug

--- src/StudentProfilePanel.h
+++ src/StudentProfilePanel.h
@@ -21,6 +21,9 @@ private:
     wxComboBox* yearCombo;
     wxComboBox* specializationCombo;
     wxCheckListBox* coursesCheckList;
+    wxListBox* completedCoursesList;
+    wxButton* addButton;
+    wxButton* removeButton;
     wxButton* saveButton;
     wxListBox* missingList;
     wxStaticText* missingLabel;
@@ -31,14 +34,17 @@ private:
     void LoadCatalog();
     void PopulateYearCombo();
     void PopulateSpecializationCombo();
-    void PopulateCoursesList();
+    void PopulateCoursesCheckList();
+    void RefreshCompletedCoursesList();
     void SyncUIFromProfile();
     void SyncProfileFromUI();
     void RefreshMissingRequirements();
-    /** @brief Updates selection status text (optionally with the course that was just toggled) */
-    void UpdateSelectionStatus(int toggledIndex = -1);
+    void UpdateSelectionStatus();
 
     void OnSave(wxCommandEvent& event);
+    void OnAddToCompleted(wxCommandEvent& event);
+    void OnRemoveFromCompleted(wxCommandEvent& event);
+    void OnCompletedCoursesDoubleClick(wxCommandEvent& event);
     void OnSelectionChanged(wxCommandEvent& event);
 };
 
